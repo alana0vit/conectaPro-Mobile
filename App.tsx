8d2a310d0686b22585546a4ed4eb571605fbf764
João Pedro da Costa <jpcc.dev@gmail.com>
2026-06-09 22:01:03 -0300

    fix: configura para respeitar o notch do celular

--- App.tsx
+++ App.tsx
@@ -4,15 +4,16 @@ import { NavigationContainer } from "@react-navigation/native"
 import AppNavigator from './src/navigation/AppNavigator';
 import Toast from 'react-native-toast-message';
 import { toastConfig } from './src/components/toastConfig';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <>
+    <SafeAreaView style={{ flex: 1 }}>
       <StatusBar style="auto" />
       <NavigationContainer>
         <AppNavigator />
       </NavigationContainer>
       <Toast config={toastConfig} />
-    </>
+    </SafeAreaView>
   );
 }
